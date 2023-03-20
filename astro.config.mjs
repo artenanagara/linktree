a3b100353b8fce276bcb9a8c3c6610dec9737d66
Artena <artenadhytyan22@gmail.com>
2023-03-20 21:08:29 +0700

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import netlify from "@astrojs/netlify/functions";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify(),
+  adapter: vercel(),
   integrations: [tailwind()]
 });
